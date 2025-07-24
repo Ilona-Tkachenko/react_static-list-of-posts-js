2b64cc4f8af8e3904cd6a0b541c39e021a453538
Ilona-Tkachenko <ilonatkachenko796@gmail.com>
2025-07-24 23:16:38 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,29 @@
-export const PostInfo = () => <>Put the post here</>;
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line import/extensions
+import { UserInfo } from './UserInfo';
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { CommentList } from './CommentList';
+import './PostInfo.scss';
+
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <UserInfo user={post.ser} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    {post.comments.length > 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,10 @@
-export const PostList = () => <>Put the list here</>;
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { PostInfo } from './PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
+);
